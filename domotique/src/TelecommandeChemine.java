0874349618f9d26ca18d8d43ae68c6119cac1b23
Segard Mathis <mathis.segard6@etu.univ-lorraine.fr>
2023-04-12 17:56:51 +0200

    ajout class cheminé

---
+++ domotique/src/TelecommandeChemine.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class TelecommandeChemine implements Telecommandable {
+    private ArrayList<Cheminee> cheminees;
+
+    @Override
+    public void allumer() {
+        
+    }
+
+    @Override
+    public void eteindre() {
+
+    }
+}
--- domotique/src/TelecommandeSpeciale.java
+++
@@ -1,5 +0,0 @@
-import java.util.ArrayList;
-
-public abstract class TelecommandeSpeciale implements Telecommandable {
-    private ArrayList<Cheminee> cheminees;
-}
